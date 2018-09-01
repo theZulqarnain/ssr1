1bc5398029ec58379e4ce36dd4b7829bb56f9bd5
zulqarnain <abdulla.zulqarnain@stanplus.co.in>
2018-09-01 23:51:07 +0530

    39

---
+++ src/client/actions/index.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+export const FETCH_USER = 'fetch_user';
+export const fetchUsers = () => async dispatch => {
+    const res = await axios.get('http://react-ssr-api.herokuapp.com/users');
+
+    dispatch({
+        type:FETCH_USER,
+        payload:res
+    })
+}
---
+++ src/client/reducers/index.js
@@ -0,0 +1,5 @@
+import {combineReducers} from 'redux';
+import usersReducer from './userReducer';
+export default combineReducers({
+    users:usersReducer
+})
