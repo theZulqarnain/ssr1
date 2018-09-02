06c1945c801a69f05297bf84d9466a9d3ecb0100
zulqarnain <abdulla.zulqarnain@stanplus.co.in>
2018-09-02 11:28:42 +0530

    49 fetching client code from server and before refactoring

--- src/client/client.js
+++ src/client/client.js
@@ -7,13 +7,14 @@ import Routes from './Routes';
 import {createStore,applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import {Provider} from 'react-redux';
+import {renderRoutes} from 'react-router-config';
 import reducers from './reducers'
 
 const store = createStore(reducers,{},applyMiddleware(thunk))
 ReactDOM.hydrate(
     <Provider store={store}>
         <BrowserRouter>
-            <Routes/>
+            <div>{renderRoutes(Routes)}</div>
         </BrowserRouter>
     </Provider>,
 document.querySelector('#root'));
